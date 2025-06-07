52a537f03b20c30f67ee9602eeb28744cbd55dad
PatrikS <patrik.stojsavljevic@elatus.net>
2025-06-07 13:40:25 +0200

    clean development project

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -1,43 +1,25 @@
-// This file is a fallback for using MaterialIcons on Android and web.
+import React from "react";
+import { StyleProp } from "react-native";
+import { Image } from "expo-image";
+import { ImageStyle as RNImageStyle } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
 
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { SymbolWeight } from 'expo-symbols';
-import React from 'react';
-import { OpaqueColorValue, StyleProp, ViewStyle } from 'react-native';
-
-// Add your SFSymbol to MaterialIcons mappings here.
-const MAPPING = {
-  // See MaterialIcons here: https://icons.expo.fyi
-  // See SF Symbols in the SF Symbols app on Mac.
-  'house.fill': 'home',
-  'paperplane.fill': 'send',
-  'chevron.left.forwardslash.chevron.right': 'code',
-  'chevron.right': 'chevron-right',
-} as Partial<
-  Record<
-    import('expo-symbols').SymbolViewProps['name'],
-    React.ComponentProps<typeof MaterialIcons>['name']
-  >
->;
-
-export type IconSymbolName = keyof typeof MAPPING;
-
-/**
- * An icon component that uses native SFSymbols on iOS, and MaterialIcons on Android and web. This ensures a consistent look across platforms, and optimal resource usage.
- *
- * Icon `name`s are based on SFSymbols and require manual mapping to MaterialIcons.
- */
 export function IconSymbol({
   name,
   size = 24,
   color,
   style,
 }: {
-  name: IconSymbolName;
+  name: string;
   size?: number;
-  color: string | OpaqueColorValue;
-  style?: StyleProp<ViewStyle>;
-  weight?: SymbolWeight;
+  color: string;
+  style?: StyleProp<RNImageStyle>;
 }) {
-  return <MaterialIcons color={color} size={size} name={MAPPING[name]} style={style} />;
+  return (
+    <Image
+      source={name}
+      style={[style, { width: size }]}
+      tintColor={color}
+      contentFit={"contain"}
+    />
+  );
 }
